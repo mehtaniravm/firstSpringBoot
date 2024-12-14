54b2423f081443308e5397dd0b05e6aae6b944e0
mehtaniravm <mehtaniravm@gmail.com>
2024-12-13 23:53:43 -0500

    add todo

--- src/main/java/com/firstspringbootapp/example/firstspringapp/controller/TodoController.java
+++ src/main/java/com/firstspringbootapp/example/firstspringapp/controller/TodoController.java
@@ -1,13 +1,17 @@
 package com.firstspringbootapp.example.firstspringapp.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.firstspringbootapp.example.firstspringapp.service.TodoService;
 import com.firstspringbootapp.example.firstspringapp.entity.todo;
 
@@ -17,9 +21,11 @@ public class TodoController {
 
     private TodoService todoService;
     
+    ////#region
     public TodoController(TodoService todoService) {
         this.todoService = todoService;
     }
+    ////#endregion
 
     @RequestMapping("list-todos")
     public String listTodos(ModelMap model) {
@@ -27,5 +33,18 @@ public class TodoController {
         model.addAttribute("todos", lstTodos);
         return "listtodos";
     }
+    
+    @RequestMapping(value="addtodos", method=RequestMethod.GET)
+    public String addTodo() {
+        return "todo";
+    }
+
+    @RequestMapping(value="addtodos", method=RequestMethod.POST)
+    public String requestMethodName(@RequestParam String description) {
+        todoService.addTodo("abc", description, LocalDate.now().plusYears(4),false);
+        return "redirect:list-todos";
+        //return new RedirectView("list-todos");
+    }
+    
     
 }
--- src/main/java/com/firstspringbootapp/example/firstspringapp/service/TodoService.java
+++ src/main/java/com/firstspringbootapp/example/firstspringapp/service/TodoService.java
@@ -9,14 +9,21 @@ import com.firstspringbootapp.example.firstspringapp.entity.todo;
 
 @Service
 public class TodoService {
-    private static List<todo> todos=new ArrayList<todo>();
-    static{
+    private static List<todo> todos = new ArrayList<todo>();
+    private static int todosCount = 3;
+    static {
         todos.add(new todo(1, "user1", "Devops", LocalDate.now().plusYears(1), false));
         todos.add(new todo(2, "user2", "GCP", LocalDate.now().plusYears(2), false));
         todos.add(new todo(3, "user3", "Full stack", LocalDate.now().plusYears(3), false));
     }
 
-    public List<todo> findByUserName(String username){
+    public List<todo> findByUserName(String username) {
         return todos;
+    }
+
+    public void addTodo(String username, String description, LocalDate targetDate, Boolean done) {
+        todosCount++;
+        todos.add(new todo(todosCount,username,description,targetDate,done));
+
     }
 }
